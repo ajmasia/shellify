f4cd35b9fe3d49ce51928ef141073bcce3023fa5
Antonio Masiá <ajmasia.dev@ysnp.link>
2026-01-08 17:42:18 +0100

    feat(cli): add output helpers

---
+++ internal/interfaces/cli/helpers.go
@@ -0,0 +1,85 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
+)
+
+// printJSON outputs data as formatted JSON.
+func printJSON(v any) error {
+	data, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return fmt.Errorf("marshaling JSON: %w", err)
+	}
+	fmt.Println(string(data))
+	return nil
+}
+
+// printTable outputs data in a formatted table.
+func printTable(headers []string, rows [][]string) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+
+	// Print headers
+	for i, h := range headers {
+		if i > 0 {
+			fmt.Fprint(w, "\t")
+		}
+		fmt.Fprint(w, h)
+	}
+	fmt.Fprintln(w)
+
+	// Print rows
+	for _, row := range rows {
+		for i, col := range row {
+			if i > 0 {
+				fmt.Fprint(w, "\t")
+			}
+			fmt.Fprint(w, col)
+		}
+		fmt.Fprintln(w)
+	}
+
+	_ = w.Flush()
+}
+
+// formatTime returns a human-readable relative time string.
+func formatTime(t time.Time) string {
+	now := time.Now()
+	diff := now.Sub(t)
+
+	switch {
+	case diff < time.Minute:
+		return "just now"
+	case diff < time.Hour:
+		mins := int(diff.Minutes())
+		if mins == 1 {
+			return "1 minute ago"
+		}
+		return fmt.Sprintf("%d minutes ago", mins)
+	case diff < 24*time.Hour:
+		hours := int(diff.Hours())
+		if hours == 1 {
+			return "1 hour ago"
+		}
+		return fmt.Sprintf("%d hours ago", hours)
+	case diff < 7*24*time.Hour:
+		days := int(diff.Hours() / 24)
+		if days == 1 {
+			return "1 day ago"
+		}
+		return fmt.Sprintf("%d days ago", days)
+	default:
+		return t.Format("2006-01-02")
+	}
+}
+
+// truncateID returns the first 8 characters of an ID.
+func truncateID(id string) string {
+	if len(id) <= 8 {
+		return id
+	}
+	return id[:8]
+}
